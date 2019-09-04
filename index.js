e40446e9c80acb36013f5b0315433be76d3691f9
Nil <nilarmengol@gmail.com>
2019-09-04 13:32:38 +0700

    080202

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const express = require('express')
 const app = express()
 
 app.listen(4000, () => { console.log('Ready on port 4000') })
+
+
+app.get('/', (req, res) => {
+	res.send(' Welcome to Airbnb API')
+})
