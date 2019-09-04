caa68f8e91ed4f287a02014cda17ce34d2f12ee5
Nil <nilarmengol@gmail.com>
2019-09-04 15:14:29 +0700

    080203

--- index.js
+++ index.js
@@ -7,3 +7,38 @@ app.listen(4000, () => { console.log('Ready on port 4000') })
 app.get('/', (req, res) => {
 	res.send(' Welcome to Airbnb API')
 })
+
+
+
+app.get('/places', (req, res) => {
+	res.send([
+  {
+    _id: '1',
+    title: 'Luxury Villa Indu Siam',
+    type: 'Entire Villa',
+    price: 200,
+    rating: 5,
+    city: 'Koh Samui',
+    country: 'Thailand',
+    amenities: ['Kitchen', 'Gym', 'Iron'],
+    images: [
+      'https://q-ak.bstatic.com/images/hotel/max1024x768/186/186223203.jpg',
+      'https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223171.jpg'
+    ]
+  },
+  {
+    _id: '2',
+    title: 'Breathtaking Infinite Pool',
+    type: 'Entire Villa',
+    price: 180,
+    rating: 4,
+    city: 'Bali',
+    country: 'Indonesia',
+    amenities: ['Air Conditioning', 'Kitchen', 'Swimming Pool'],
+    images: [
+      'https://q-ak.bstatic.com/images/hotel/max1024x768/186/186223203.jpg',
+      'https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223171.jpg'
+    ]
+  }
+])
+})
