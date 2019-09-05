4267ba1095643f0a12e38200b5bbcd8c26b5c48b
Nil <nilarmengol@gmail.com>
2019-09-05 17:05:10 +0700

    090401

--- controllers/getPlaces.js
+++ controllers/getPlaces.js
@@ -1,33 +1,12 @@
+const Place = require('../models/place')
 
 module.exports = (req, res) => {
-	res.send([
-  {
-    _id: '1',
-    title: 'Luxury Villa Indu Siam',
-    type: 'Entire Villa',
-    price: 200,
-    rating: 5,
-    city: 'Koh Samui',
-    country: 'Thailand',
-    amenities: ['Kitchen', 'Gym', 'Iron'],
-    images: [
-      'https://q-ak.bstatic.com/images/hotel/max1024x768/186/186223203.jpg',
-      'https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223171.jpg'
-    ]
-  },
-  {
-    _id: '2',
-    title: 'Breathtaking Infinite Pool',
-    type: 'Entire Villa',
-    price: 180,
-    rating: 4,
-    city: 'Bali',
-    country: 'Indonesia',
-    amenities: ['Air Conditioning', 'Kitchen', 'Swimming Pool'],
-    images: [
-      'https://q-ak.bstatic.com/images/hotel/max1024x768/186/186223203.jpg',
-      'https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223171.jpg'
-    ]
-  }
-])
+
+	Place.find({}).then(data => {
+		 res.send(data)
+	})
+	.catch(err => {
+		res.send(err)
+	})
+
 }
