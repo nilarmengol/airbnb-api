8e0ea7593860e028ea140622ce0060dfc90ca229
Nil <nilarmengol@gmail.com>
2019-09-06 15:42:26 +0700

    090901

--- controllers/getPlaces.js
+++ controllers/getPlaces.js
@@ -1,8 +1,17 @@
 
 const Place = require('../models/place')
 
+// module.exports = (req, res) => {
+// 	Place.find({}).then(data => {
+// 		 res.send(data)
+// 	})
+// 	.catch(err => {
+// 		res.send(err)
+// 	})
+// }
+
 module.exports = (req, res) => {
-	Place.find({}).then(data => {
+	Place.findbyId(req.params.id).select('-price').then(data => {
 		 res.send(data)
 	})
 	.catch(err => {
