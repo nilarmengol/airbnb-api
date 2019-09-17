78198caa013b0b12ed6c7901863bb3c5f68a26bc
Nil <nilarmengol@gmail.com>
2019-09-17 11:01:19 +0700

    140102

--- controllers/postPay.js
+++ controllers/postPay.js
@@ -1,8 +1,18 @@
 const stripe = require("stripe")(process.env.SECRET_STRIPE_SK)
 
+
+module.exports = (req, res) => {
+
 stripe.charges.create({
   amount: 20000,
   currency: 'usd',
   description: '',
   source: token
-}).then(data => {})
+}).then(data => {
+	 res.send(data)
+})
+.catch(err => {
+	res.send(err)
+})
+
+}
